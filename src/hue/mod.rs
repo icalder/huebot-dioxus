4020b651994ea9747363f661963ac7eb2a5e32d6
Iain Calder <iain.calder@gmail.com>
2026-01-24 20:35:03 +0000

    feat: use env vars for hue bridge

--- src/hue/mod.rs
+++ src/hue/mod.rs
@@ -6,13 +6,13 @@ pub mod tests;
 
 #[cfg(feature = "server")]
 static HUE_CLIENT: LazyLock<client::ClientEx> = LazyLock::new(|| {
-    let ip = "192.168.1.107";
-    let key = "yAqYgN-3scCv858Ed5YIvWqONSSBo-7IMOUIuqNE";
+    let ip = std::env::var("HUE_IP").expect("HUE_IP environment variable must be set");
+    let key = std::env::var("HUE_KEY").expect("HUE_KEY environment variable must be set");
 
     let mut headers = reqwest::header::HeaderMap::new();
     headers.insert(
         "hue-application-key",
-        reqwest::header::HeaderValue::from_str(key).unwrap(),
+        reqwest::header::HeaderValue::from_str(&key).unwrap(),
     );
 
     let reqwest_client = reqwest::Client::builder()
